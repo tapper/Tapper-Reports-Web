ed94d60702bd7e6cbe82e8ad39ed9f822a58fc43
Steffen Schwigon <ss5@renormalist.net>
2012-08-07 17:54:18 +0200

    get rid of NEXT
    - in MyApp at least
      (we still use ::BindLex which has NEXT in it)
    - carve out behaviour modification into role
    - the ConfigLoader stuff is dropped here and
      will return soon

--- lib/Tapper/Reports/Web.pm
+++ lib/Tapper/Reports/Web.pm
@@ -1,70 +1,23 @@
 package Tapper::Reports::Web;
 # ABSTRACT: Tapper - Frontend web application based on Catalyst
 
+use 5.010;
 use strict;
 use warnings;
 
-use 5.010;
+use Moose;
+use Catalyst::Runtime;
 
-use Catalyst::Runtime '5.70';
-use Hash::Merge;
+extends 'Catalyst';
+with 'Tapper::Reports::Web::Role::BehaviourModifications::Path';
+
 use Tapper::Config;
 use File::ShareDir ':ALL';
-
-use Class::C3::Adopt::NEXT;
-
-# Set flags and add plugins for the application
-#
-#         -Debug: activates the debug mode for very useful log messages
-#   ConfigLoader: will load the configuration from a Config::General file in the
-#                 application's home directory
-# Static::Simple: will serve static files from the application's root
-#                 directory
-
-use parent qw/Catalyst/;
-
-# used by Catalyst::Plugin::ConfigLoader
-sub finalize_config
-{
-        my $c = shift;
-
-        $c->NEXT::ACTUAL::finalize_config;
-        my $env =
-            $ENV{HARNESS_ACTIVE}                 ? 'test'
-                : $ENV{TAPPER_REPORTS_WEB_LIVE} ? 'live'
-                    : 'development';
-        Hash::Merge::set_behavior('RIGHT_PRECEDENT');
-        $c->config(
-                   Hash::Merge::merge(
-                                      $c->config,
-                                      $c->config->{ $env } || {} ,
-                                     )
-                  );
-        $c->config->{tapper_config} = Tapper::Config->subconfig;
-
-        return;
-}
 
 sub debug
 {
         return $ENV{TAPPER_REPORTS_WEB_LIVE} || $ENV{HARNESS_ACTIVE} ? 0 : 1;
 }
-
-# I am sick of getting relocated/rebase on our local path!
-# Cut away a trailing 'tapper/' from base and prepend it to path.
-# All conditionally only when this annoying environment is there.
-sub prepare_path
-{
-        my $c = shift;
-
-        $c->NEXT::prepare_path(@_);
-
-        my $base        =  $c->req->{base}."";
-        $base           =~ s,tapper/$,, if $base;
-        $c->req->{base} =  bless( do{\(my $o = $base)}, 'URI::http' );
-        $c->req->path('tapper/'.$c->req->path) unless ( $c->req->path =~ m,^tapper/?,);
-}
-
 
 # Configure the application.
 __PACKAGE__->config( name => 'Tapper::Reports::Web',
