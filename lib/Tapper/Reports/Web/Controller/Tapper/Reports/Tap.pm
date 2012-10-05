b0ec52d92c47a63504dfa322d0c50812bd92a0d9
Steffen Schwigon <steffen.schwigon@amd.com>
2012-10-05 17:03:14 +0200

    take care of nonexistent values

--- lib/Tapper/Reports/Web/Controller/Tapper/Reports/Tap.pm
+++ lib/Tapper/Reports/Web/Controller/Tapper/Reports/Tap.pm
@@ -12,14 +12,14 @@ sub index :Path :Args(1)
         my $report = $c->model('ReportsDB')->resultset('Report')->find($report_id);
 
         if ($report) {
-                if ($report->tap->tap_is_archive) {
+                if ($report->tap && $report->tap->tap_is_archive) {
                         $c->response->content_type ('application/x-compressed');
                         $c->response->header ("Content-Disposition" => 'inline; filename="tap-'.$report_id.'.tgz"');
                 } else {
                         $c->response->content_type ('text/plain');
                         $c->response->header ("Content-Disposition" => 'inline; filename="tap-'.$report_id.'.tap"');
                 }
-                $c->response->body ($report->tap->tap || "Error: No TAP for report $report_id.");
+                $c->response->body ($report->tap ? $report->tap->tap : "Error: No TAP for report $report_id.");
         } else {
                 $c->response->content_type ("text/plain");
                 $c->response->header ("Content-Disposition" => 'inline; filename="nonexistent.report.tap.'.$report_id.'"');
