430bdcf5574b3260ff870f3a6e9094867808fc9a
Roberto Schaefer <schaefr@amazon.com>
2013-09-18 14:24:36 +0200

    add overview line, shrink header part of page

--- lib/Tapper/Reports/Web/Controller/Tapper/Reports.pm
+++ lib/Tapper/Reports/Web/Controller/Tapper/Reports.pm
@@ -68,6 +68,18 @@ sub prepare_this_weeks_reportlists : Private {
 
         $or_c->stash->{pager_interval}  = $hr_params->{pager_interval} || 1;
         $or_c->stash->{report_date}     = $hr_filter_condition->{report_date};
+
+        # set reports date
+        my $d_report_date_from = $hr_filter_condition->{report_date}->clone->subtract( days => $or_c->stash->{pager_interval} - 1 )->strftime('%d %b %Y');
+        my $d_report_date_to   = $hr_filter_condition->{report_date}->strftime('%d %b %Y');
+
+        if ( $d_report_date_from ne $d_report_date_to ) {
+            $or_c->stash->{head_overview}   = "Testruns ( $d_report_date_to - $d_report_date_from )";
+        }
+        else {
+            $or_c->stash->{head_overview}   = "Testruns ( $d_report_date_from )";
+        }
+
         $or_c->stash->{reports}         = $or_c->model('TestrunDB')->fetch_raw_sql({
             query_name  => 'reports::web_list',
             fetch_type  => '@%',
--- lib/Tapper/Reports/Web/Controller/Tapper/Testruns.pm
+++ lib/Tapper/Reports/Web/Controller/Tapper/Testruns.pm
@@ -593,6 +593,18 @@ sub prepare_testrunlists : Private {
 
         $or_c->stash->{pager_interval}  = $hr_params->{pager_interval} || 1;
         $or_c->stash->{testrun_date}    = $hr_filter_condition->{testrun_date};
+
+        # set testrun date
+        my $d_testrun_date_from = $hr_filter_condition->{testrun_date}->clone->subtract( days => $or_c->stash->{pager_interval} - 1 )->strftime('%d %b %Y');
+        my $d_testrun_date_to   = $hr_filter_condition->{testrun_date}->strftime('%d %b %Y');
+
+        if ( $d_testrun_date_from ne $d_testrun_date_to ) {
+            $or_c->stash->{head_overview}   = "Testruns ( $d_testrun_date_to - $d_testrun_date_from )";
+        }
+        else {
+            $or_c->stash->{head_overview}   = "Testruns ( $d_testrun_date_from )";
+        }
+
         $or_c->stash->{testruns}        = $or_c->model('TestrunDB')->fetch_raw_sql({
                 query_name  => 'testruns::web_list',
                 fetch_type  => '@%',
